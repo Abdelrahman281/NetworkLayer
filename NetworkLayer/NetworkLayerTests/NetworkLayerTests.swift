518b3c707feb283c3968902ff4e745dee9a2938a
Abdelrahman <abdelrahman.mahmoud.r92@gmail.com>
2019-12-03 12:40:01 +0200

    ADD: User Model

--- NetworkLayer/NetworkLayerTests/NetworkLayerTests.swift
+++ NetworkLayer/NetworkLayerTests/NetworkLayerTests.swift
@@ -10,25 +10,26 @@ import XCTest
 @testable import NetworkLayer
 
 class NetworkLayerTests: XCTestCase {
-
+    
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testCallDataService() {
+        
+        let urlPath = "https://testingq.getsandbox.com/User"
+        
+        let promise = expectation(description: "StatusCode 200")
+        
+       NetworkLayer.shared.callDataService(urlPath: urlPath, method: HTTPMethod.GET, timeOutInterval: 30.0, responseClass: User, success: { obj in
+        Swift.print("hello")
+       }, failure: {error in
+        Swift.print(error)
+       })
+        
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
---
+++ NetworkLayer/NetworkLayerTests/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  NetworkLayerTests
+//
+//  Created by Abdelrahman Mahmoud on 11/18/19.
+//  Copyright © 2019 AbdEl-Rahman Mahmoud. All rights reserved.
+//
+
+import Foundation
+
+class User: Decodable {
+    var iD: String
+    var name: String
+    var age: String
+}
